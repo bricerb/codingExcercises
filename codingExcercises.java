8fcc333593dfcbf79da9156f628d21a070b33d32
Richard Blanch <bricerb@gmail.com>
2016-11-02 11:56:30 -0400

    Sovled powerN

--- codingExcercises.java
+++ codingExcercises.java
@@ -189,3 +189,27 @@ public int fastCount8(int n, int tracker, boolean previous8) {
   }
   return fastCount8(n / 10, tracker, previous8);
 }
+
+
+
+/*
+Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+powerN(3, 1) → 3
+powerN(3, 2) → 9
+powerN(3, 3) → 27
+*/
+
+public int powerN(int base, int n) {
+  if (n == 1) {
+    return base;
+  }
+  
+  n -= 1;
+  int basePower = base;
+  return base * powerN(base, n);
+}
+
+
+
+

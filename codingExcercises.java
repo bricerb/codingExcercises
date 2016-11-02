a9af85312f76cbe2d004c0b4021077c3f854232b
Richard Blanch <bricerb@gmail.com>
2016-11-02 11:43:05 -0400

    Solved count7

--- codingExcercises.java
+++ codingExcercises.java
@@ -134,6 +134,29 @@ public int fastSumDigits(int n, int tracker) {
 
 
 
+/*
+
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+count7(717) → 2
+count7(7) → 1
+count7(123) → 0
+*/
+
+public int count7(int n) {
+  return effCount7(n, 0);
+}
+
+public int effCount7(int n, int tracker) {
+  if (n == 0) {
+    return tracker;
+  }
+  
+  if (n % 10 == 7) {
+    tracker += 1;
+  }
+  return effCount7(n / 10, tracker);
+}
 
 
 

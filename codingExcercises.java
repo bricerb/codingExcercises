1030144e99b9e42394185e9b417d9d8bc0698e9e
Richard Blanch <bricerb@gmail.com>
2016-11-02 11:50:15 -0400

    Solved count8

--- codingExcercises.java
+++ codingExcercises.java
@@ -160,3 +160,32 @@ public int effCount7(int n, int tracker) {
 
 
 
+/*
+Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+count8(8) → 1
+count8(818) → 2
+count8(8818) → 4
+*/
+
+public int count8(int n) {
+  return fastCount8(n, 0, false);
+}
+
+public int fastCount8(int n, int tracker, boolean previous8) {
+  if (n == 0) {
+    return tracker;
+  }
+  
+  if (n % 10 == 8) {
+    if (previous8 == false) {
+      tracker += 1;
+      previous8 = true;
+    } else {
+      tracker += 2;
+    }
+  } else {
+    previous8 = false;
+  }
+  return fastCount8(n / 10, tracker, previous8);
+}

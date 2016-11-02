e45e54f66e01db5687a01f7b3dd87efc7b3c3bc2
Richard Blanch <bricerb@gmail.com>
2016-11-02 10:38:42 -0400

    Solved fibonacci

--- codingExcercises.java
+++ codingExcercises.java
@@ -30,3 +30,30 @@ public int factorial(int n) {
   o = n;
   return o * factorial(n-1);
 }
+
+
+/*
+The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+
+fibonacci(0) → 0
+fibonacci(1) → 1
+fibonacci(2) → 1
+*/
+
+int trackerNumber = 0;
+public int fibonacci(int n) {
+  return fastFib(0, 1, n);
+}
+
+public int fastFib(int firstFib, int secondFib, int fibTracker) {
+  trackerNumber++;
+  if (fibTracker == 0) {
+    return 0;
+  }
+  if (fibTracker == 1) {
+      return secondFib;
+  }
+  int currentFibNumber = firstFib + secondFib;
+  fibTracker--;
+  return fastFib(secondFib, currentFibNumber, fibTracker);
+}

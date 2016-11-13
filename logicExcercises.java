7b6bc3e560f6277937e29e0617082d81af8c318e
Richard Blanch <bricerb@gmail.com>
2016-11-13 15:52:38 -0500

    Solved lessBy10

--- logicExcercises.java
+++ logicExcercises.java
@@ -332,3 +332,7 @@ public boolean lastDigit(int a, int b, int c) {
 /* Given three ints, a b c, return true if one of them is 10 or more less
 than one of the others. */
 
+public boolean lessBy10(int a, int b, int c) {
+  return (Math.abs(a - b) >= 10 || Math.abs(a - c) >= 10 || Math.abs(b - c) >= 10);
+}
+

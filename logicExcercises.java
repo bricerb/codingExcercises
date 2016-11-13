ece02ff2073b8d3aa5e68de078cd06133106294b
Richard Blanch <bricerb@gmail.com>
2016-11-13 14:18:16 -0500

    Sovled caughtSpeeding

--- logicExcercises.java
+++ logicExcercises.java
@@ -65,3 +65,32 @@ public boolean squirrelPlay(int temp, boolean isSummer) {
     } 
   }
 }
+
+/* You are driving a little too fast, and a police officer stops you.
+Write code to compute the result, encoded as an int value: 0=no
+ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the
+result is 0. If speed is between 61 and 80 inclusive, the result is 1.
+If speed is 81 or more, the result is 2. Unless it is your birthday --
+on that day, your speed can be 5 higher in all cases. */
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  if (isBirthday) {
+    if (speed <= 65) {
+      return 0;
+    } else if (speed > 65 && speed <= 85) {
+      return 1;
+    } else if (speed > 85) {
+      return 2;
+    }
+  } else {
+    if (speed <= 60) {
+      return 0;
+    } else if (speed > 60 && speed <= 80) {
+      return 1;
+    } else if (speed > 80) {
+      return 2;
+    }
+  } 
+  return 0;
+}
+

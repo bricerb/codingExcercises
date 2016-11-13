c84050257a333b9f77b6c1213ee07dd45e7cbf21
Richard Blanch <bricerb@gmail.com>
2016-11-13 15:57:12 -0500

    Solved withoutDoubles

--- logicExcercises.java
+++ logicExcercises.java
@@ -336,3 +336,21 @@ public boolean lessBy10(int a, int b, int c) {
   return (Math.abs(a - b) >= 10 || Math.abs(a - c) >= 10 || Math.abs(b - c) >= 10);
 }
 
+/* Return the sum of two 6-sided dice rolls, each in the range 1..6.
+However, if noDoubles is true, if the two dice show the same value,
+increment one die to the next value, wrapping around to 1 if its value
+was 6. */
+
+public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+  if (noDoubles) {
+    if (die1 == 6) {
+      return 7;
+    } else if (die1 == die2) {
+      return (die1 + die2 + 1);
+    } else {
+      return (die1 + die2);
+    }
+  }
+  return (die1 + die2);
+}
+

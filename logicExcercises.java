f1df90baaefdd8b3cb5263a4b95f59a14634e81e
Richard Blanch <bricerb@gmail.com>
2016-11-13 15:51:06 -0500

    solved lastDigit

--- logicExcercises.java
+++ logicExcercises.java
@@ -319,3 +319,16 @@ public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
   }
   return (a < b && b < c);
 }
+
+
+/* Given three ints, a b c, return true if two or more of them have the
+same rightmost digit. The ints are non-negative. Note: the % "mod"
+operator computes the remainder, e.g. 17 % 10 is 7. */
+
+public boolean lastDigit(int a, int b, int c) {
+  return (a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10);
+}
+
+/* Given three ints, a b c, return true if one of them is 10 or more less
+than one of the others. */
+

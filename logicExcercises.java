10a8fd4461946ed2abc864b755b8a72ca8531dcb
Richard Blanch <bricerb@gmail.com>
2016-11-13 14:48:02 -0500

    Solved old35

--- logicExcercises.java
+++ logicExcercises.java
@@ -179,6 +179,16 @@ public boolean more20(int n) {
 }
 
 
+/* Return true if the given non-negative number is a multiple of 3 or 5,
+but not both. Use the % "mod" operator */
+
+public boolean old35(int n) {
+  if (n % 3 == 0 && n % 5 == 0) {
+    return false;
+  } else {
+    return (n % 3 == 0 || n % 5 == 0);
+  }
+}
 
 
 

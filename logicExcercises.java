8d3a693e847ea8f061dc96156466bd7805314dea
Richard Blanch <bricerb@gmail.com>
2016-11-13 14:13:59 -0500

    solved squirrelPlay

--- logicExcercises.java
+++ logicExcercises.java
@@ -41,3 +41,27 @@ public int dateFashion(int you, int date) {
   }
 }
 
+
+
+
+/* The squirrels in Palo Alto spend most of the day playing. In
+ particular, they play if the temperature is between 60 and 90
+ (inclusive). Unless it is summer, then the upper limit is 100
+ instead of 90. Given an int temperature and a boolean isSummer
+ return true if the squirrels play and false otherwise. */
+
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if (isSummer) {
+    if (temp >= 60 && temp <= 100) {
+      return true;
+    } else {
+      return false;
+    } 
+  } else {
+    if (temp >= 60 && temp <= 90) {
+      return true;
+    } else {
+      return false;
+    } 
+  }
+}

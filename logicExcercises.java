bbfaa494e4b663f790d1b3dfbc6a7bbe2dd3b3ba
Richard Blanch <bricerb@gmail.com>
2016-11-13 14:51:24 -0500

    Solved nearTen

--- logicExcercises.java
+++ logicExcercises.java
@@ -198,3 +198,13 @@ public boolean less20(int n) {
   return (n % 20 == 18 || n % 20 == 19);
 }
 
+/* Given a non-negative number "num", return true if num is within 2 of a
+multiple of 10. Note: (a % b) is the remainder of dividing a by b, so
+(7 % 5) is 2. */
+
+public boolean nearTen(int num) {
+  return (num % 10 <= 2 || num % 10 >= 8);
+}
+
+
+

5026d2591edc4f27ab1877da8aa228357ca373e1
Richard Blanch <bricerb@gmail.com>
2016-11-13 14:44:05 -0500

    Solved specialEleven

--- logicExcercises.java
+++ logicExcercises.java
@@ -162,3 +162,15 @@ public boolean in1To10(int n, boolean outsideMode) {
     return (n >= 1 && n <= 10);
   }
 }
+
+/* We'll say a number is special if it is a multiple of 11 or if it is
+one more than a multiple of 11. Return true if the given non-negative
+number is special. Use the % "mod" operator */
+
+public boolean specialEleven(int n) {
+  return (n % 11 == 0 || n % 11 == 1);
+}
+
+
+
+

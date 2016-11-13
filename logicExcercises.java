e6a500ddb338edccf1451e363d864255155c43b9
Richard Blanch <bricerb@gmail.com>
2016-11-13 15:47:23 -0500

    Solved inOrder

--- logicExcercises.java
+++ logicExcercises.java
@@ -295,3 +295,16 @@ the ints to get the third. */
 public boolean twoAsOne(int a, int b, int c) {
   return (a + b == c || a + c == b || b + c == a);
 }
+
+
+/* Given three ints, a b c, return true if b is greater than a, and c is
+greater than b. However, with the exception that if "bOk" is true, b
+does not need to be greater than a. */
+
+public boolean inOrder(int a, int b, int c, boolean bOk) {
+  if (bOk) {
+    return (c > b);
+  }
+  return (c > b && b > a);
+}
+

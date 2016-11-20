e0c8a6b9811492d002c9c1b61798aa303d5033a9
Richard Blanch <bricerb@gmail.com>
2016-11-20 12:38:53 -0500

    Solved makeAbba

--- String-1.java
+++ String-1.java
@@ -5,3 +5,9 @@ public String helloName(String name) {
 }
 
 
+/* 
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi". */
+
+public String makeAbba(String a, String b) {
+  return (a + b + b + a);
+}

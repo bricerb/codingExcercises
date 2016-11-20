762197111b2e6b9f25f0fcf27aabd96efb8c9d91
Richard Blanch <bricerb@gmail.com>
2016-11-20 13:26:34 -0500

    Solved middleTwp

--- String-1.java
+++ String-1.java
@@ -111,3 +111,8 @@ public String withouEnd2(String str) {
   return (str.substring(1, str.length() - 1));
 }
 
+/* Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2. */
+
+public String middleTwo(String str) {
+  return (str.substring(str.length() / 2 - 1, str.length() / 2 + 1));
+}

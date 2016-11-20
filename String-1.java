5dd5892ef5ff6137a20492c3a263219ae22aaa04
Richard Blanch <bricerb@gmail.com>
2016-11-20 12:58:39 -0500

    Solved firstTwo

--- String-1.java
+++ String-1.java
@@ -33,3 +33,13 @@ public String extraEnd(String str) {
   return (str.substring(str.length() - 2, str.length()) + str.substring(str.length() - 2, str.length()) + str.substring(str.length() - 2, str.length()));
 }
 
+/* Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+ */
+
+public String firstTwo(String str) {
+  if (str.length() < 2) {
+    return str;
+  }
+  return (str.substring(0,2));
+}
+

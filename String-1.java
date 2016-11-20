ebf1ba4e300e65754dee06d9867048146a868613
Richard Blanch <bricerb@gmail.com>
2016-11-20 13:02:00 -0500

    Solved firstHalf

--- String-1.java
+++ String-1.java
@@ -43,3 +43,8 @@ public String firstTwo(String str) {
   return (str.substring(0,2));
 }
 
+/* Given a string of even length, return the first half. So the string "WooHoo" yields "Woo". */
+
+public String firstHalf(String str) {
+  return (str.substring(0, str.length() / 2));
+}

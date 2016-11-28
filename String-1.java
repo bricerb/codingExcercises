e15dcf0e7b076c728dd93becc939e78d7767ba0b
Richard Blanch <bricerb@gmail.com>
2016-11-28 07:36:12 -0500

    Solved 3 problems

--- String-1.java
+++ String-1.java
@@ -153,6 +153,25 @@ public String middleThree(String str) {
 /* 
 Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0. Note: use .equals() to compare 2 strings. */
 
+public boolean hasBad(String str) {
+  if (str.length() < 3) {
+    return false;
+  }
+  if (str.length() == 3) {
+    return (str.substring(0,3).equals("bad"));
+  }
+  
+  return (str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad"));
+}
 
+/* Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars. */
 
-
+public String atFirst(String str) {
+  if (str.length() == 1) {
+    return str + "@";
+  }
+  if (str.length() == 0) {
+    return "@@";
+  }
+  return str.substring(0,2);
+}

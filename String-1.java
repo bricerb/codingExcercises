11397a79a78837efa8e02ba61417f621f954ee35
Richard Blanch <bricerb@gmail.com>
2016-12-05 13:54:00 -0500

    Solve frontAgain

--- String-1.java
+++ String-1.java
@@ -230,6 +230,18 @@ public String seeColor(String str) {
   return "";
 }
 
+/* Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited". */
+
+public boolean frontAgain(String str) {
+  if (str.length() < 2) {
+    return false;
+  }
+  if (str.substring(0,2).equals(str.substring(str.length() - 2, str.length()))) {
+    return true;
+  }
+  return false;
+}
+
 
 
 

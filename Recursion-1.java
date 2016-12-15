4d103caf731519425af2da151edd44b0b719829d
Richard Blanch <bricerb@gmail.com>
2016-12-15 14:59:41 -0500

    Solved countAbc

--- Recursion-1.java
+++ Recursion-1.java
@@ -459,3 +459,19 @@ public int countPairs(String str) {
   }
   return 0 + countPairs(str.substring(1));
 }
+
+/* Count recursively the total number of "abc" and "aba" substrings that appear in the given string.
+
+countAbc("abc") → 1
+countAbc("abcxxabc") → 2
+countAbc("abaxxaba") → 2 */
+
+public int countAbc(String str) {
+  if (str.length() <= 2) {
+    return 0;
+  }
+  if (str.substring(0,3).equals("abc") || str.substring(0,3).equals("aba")) {
+    return 1 + countAbc(str.substring(1));
+  }
+  return 0 + countAbc(str.substring(1));
+}

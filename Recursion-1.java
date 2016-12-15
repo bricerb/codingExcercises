7e6331cc7e2a20f022f75d6eef64ca377442736a
Richard Blanch <bricerb@gmail.com>
2016-12-15 14:57:14 -0500

    Solved countPairs

--- Recursion-1.java
+++ Recursion-1.java
@@ -443,3 +443,19 @@ public String endX(String str) {
   }
   return str.substring(0,1) + endX(str.substring(1));
 }
+
+/* We'll say that a "pair" in a string is two instances of a char separated by a char. So "AxA" the A's make a pair. Pair's can overlap, so "AxAxA" contains 3 pairs -- 2 for A and 1 for x. Recursively compute the number of pairs in the given string.
+
+countPairs("axa") → 1
+countPairs("axax") → 2
+countPairs("axbx") → 1 */
+
+public int countPairs(String str) {
+  if (str.length() <= 2) {
+    return 0;
+  }
+  if (str.substring(0,1).equals(str.substring(2,3))) {
+    return 1 + countPairs(str.substring(1));
+  }
+  return 0 + countPairs(str.substring(1));
+}

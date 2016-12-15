c0d59df9414771ef06a23a81de361dd56b1ebc84
Richard Blanch <bricerb@gmail.com>
2016-12-14 20:21:48 -0500

    Solved pairStar

--- Recursion-1.java
+++ Recursion-1.java
@@ -402,3 +402,27 @@ public boolean array220(int[] nums, int index) {
   }
   return array220(nums, index+1);
 }
+
+/* Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*". */
+
+public String allStar(String str) {
+  if (str.length() == 0) {
+    return "";
+  }
+  if (str.length() == 1) {
+    return str;
+  }
+  return str.substring(0, 1) + "*" + allStar(str.substring(1));
+}
+
+/* Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*". */
+
+public String pairStar(String str) {
+  if (str.length() == 0 || str.length() == 1) {
+    return str;
+  }
+  if (str.substring(0,1).equals(str.substring(1,2))) {
+    return str.substring(0,1) + "*" + pairStar(str.substring(1));
+  }
+  return str.substring(0,1) + pairStar(str.substring(1));
+}

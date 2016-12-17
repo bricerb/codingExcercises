8bdb97c71a046cc5ed52210afc8d2b245ccfcd7f
Richard Blanch <bricerb@gmail.com>
2016-12-16 21:15:18 -0500

    Solved countHi2

--- Recursion-1.java
+++ Recursion-1.java
@@ -509,3 +509,24 @@ public String stringClean(String str) {
   return str.substring(0,1) + stringClean(str.substring(1));
 }
 
+/* Given a string, compute recursively the number of times lowercase "hi" appears in the string, however do not count "hi" that have an 'x' immedately before them.
+
+countHi2("ahixhi") → 1
+countHi2("ahibhi") → 2
+countHi2("xhixhi") → 0 */
+
+public int countHi2(String str) {
+  if (str.length() < 2) {
+    return 0;
+  }
+  if (str.substring(0,1).equals("x")) {
+    if (str.substring (1,2).equals ("x")) {
+      return 0 + countHi2(str.substring(1));
+    }
+    return 0 + countHi2(str.substring(2));
+  }
+  if (str.substring(0,2).equals("hi")) {
+    return 1 + countHi2 (str.substring(2));
+  }
+  return 0 + countHi2 (str.substring(1));
+}

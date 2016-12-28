0bcc0ffcb851491299df9099e0fb85cb000f2681
Richard Blanch <bricerb@gmail.com>
2016-12-28 17:05:40 -0500

    Solved strDist

--- Recursion-1.java
+++ Recursion-1.java
@@ -583,4 +583,20 @@ public boolean strCopies(String str, String sub, int n) {
   }
 }
 
+// Given a string and a non-empty substring sub, compute recursively the largest substring which starts and ends with sub and return its length.
+
+public int strDist(String str, String sub) {
+  if (str.length() < sub.length()) {
+    return 0;
+  }
+  if (str.substring(0, sub.length()).equals(sub)) {
+    if (str.substring(str.length() - sub.length(),str.length()).equals(sub)) {
+      return str.length();
+    } else {
+      return strDist(str.substring(0, str.length() - 1), sub);
+    }
+  }
+  return strDist(str.substring(1), sub);
+}
+
 

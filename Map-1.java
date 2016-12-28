eae0874c324dc6ce0d8f32246722a2bbf7097e34
Richard Blanch <bricerb@gmail.com>
2016-12-28 17:19:26 -0500

    Solved mapAB

--- Map-1.java
+++ Map-1.java
@@ -8,7 +8,6 @@ public Map<String, String> mapBully(Map<String, String> map) {
   return map;
 }
 
-
 // Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
 
 public Map<String, String> mapShare(Map<String, String> map) {
@@ -20,4 +19,19 @@ public Map<String, String> mapShare(Map<String, String> map) {
   }
   return map;
 }
+
+// Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys. If both keys are present, append their 2 string values together and store the result under the key "ab".
+
+public Map<String, String> mapAB(Map<String, String> map) {
+  if (map.containsKey("a") && map.containsKey("b")) {
+    map.put("ab", map.get("a") + map.get("b"));
+  }
+  return map;
+}
+
+
+
+
+
+
 

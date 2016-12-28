d3ae265ef40494e74d44c74d7320c47ddb7f699c
Richard Blanch <bricerb@gmail.com>
2016-12-28 12:10:27 -0500

    Solved strCount

--- Recursion-1.java
+++ Recursion-1.java
@@ -554,3 +554,19 @@ public boolean nestParen(String str) {
   return nestParen(str.substring(1, str.length() - 1));
 }
 
+// Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+
+public int strCount(String str, String sub) {
+  if (str.length() < sub.length()) {
+    return 0;
+  }
+  if (str.substring(0, sub.length()).equals(sub)) {
+    return 1 + strCount(str.substring(sub.length()), sub);
+  } else {
+    return 0 + strCount(str.substring(1), sub);
+  }
+}
+
+
+
+

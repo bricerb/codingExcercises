96117656fe6635c4112b7d55edde81d2a38c1eab
Richard Blanch <bricerb@gmail.com>
2016-12-30 21:13:32 -0500

    Solved middleWay

--- Array-1.java
+++ Array-1.java
@@ -76,7 +76,14 @@ public int sum2(int[] nums) {
   return nums[0] + nums[1];
 }
 
-//
+// Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+public int[] middleWay(int[] a, int[] b) {
+  int[] c = new int[2];
+  c[0] = a[1];
+  c[1] = b[1];
+  return c;
+}
 
 
 

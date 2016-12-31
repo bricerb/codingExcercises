a494f27f5133ad773f85c70a8e6ec628908088de
Richard Blanch <bricerb@gmail.com>
2016-12-30 20:49:01 -0500

    Solved commonEnd

--- Array-1.java
+++ Array-1.java
@@ -22,3 +22,11 @@ public int[] makePi() {
   pi[2] = 4;
   return pi;
 }
+
+// Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+
+public boolean commonEnd(int[] a, int[] b) {
+  return (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]);
+}
+
+

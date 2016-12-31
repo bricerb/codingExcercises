2c84cc081b8a18ccb6450b7fddf0c107c2ca9941
Richard Blanch <bricerb@gmail.com>
2016-12-30 20:47:11 -0500

    Solved makePi

--- Array-1.java
+++ Array-1.java
@@ -15,4 +15,10 @@ public boolean sameFirstLast(int[] nums) {
 
 // Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 
-
+public int[] makePi() {
+  int[] pi = new int[3];
+  pi[0] = 3;
+  pi[1] = 1;
+  pi[2] = 4;
+  return pi;
+}

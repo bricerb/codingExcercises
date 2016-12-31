43f6d8414b359fb8126bfd81655566aae397e3f2
Richard Blanch <bricerb@gmail.com>
2016-12-30 20:42:10 -0500

    Solved sameFirstLast

--- Map-1.java
+++ Map-1.java
@@ -72,6 +72,11 @@ public boolean firstLast6(int[] nums) {
   return false;
 }
 
+// Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+
+public boolean sameFirstLast(int[] nums) {
+  return (nums.length >= 1 && nums[0] == nums[nums.length - 1]);
+}
 
 
 

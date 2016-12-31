b2a1094258189567731011af21401e763be4304c
Richard Blanch <bricerb@gmail.com>
2016-12-30 20:40:13 -0500

    Solved fistLast6

--- Map-1.java
+++ Map-1.java
@@ -63,6 +63,14 @@ public Map<String, String> topping3(Map<String, String> map) {
   return map;
 }
 
+// Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+public boolean firstLast6(int[] nums) {
+  if (nums[0] == 6 || nums[nums.length - 1] == 6) {
+    return true;
+  }
+  return false;
+}
 
 
 

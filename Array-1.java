7a547c8bc3fa7940064191b6d45f58f621c64a38
Richard Blanch <bricerb@gmail.com>
2017-01-01 20:14:21 -0500

    Solved biggerTwo

--- Array-1.java
+++ Array-1.java
@@ -135,6 +135,31 @@ public int[] fix23(int[] nums) {
   return nums;
 }
 
+// Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+public int start1(int[] a, int[] b) {
+  int res = 0;
+  if (a.length != 0) {
+    if (a[0] == 1) {
+      res ++;
+    }
+  }
+  if (b.length != 0) {
+    if (b[0] == 1) {
+      res ++;
+    }
+  }
+  return res;
+}
+
+// Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+
+public int[] biggerTwo(int[] a, int[] b) {
+  if (b[0] + b[1] > a[0] + a[1]) {
+    return b;
+  }
+  return a;
+}
 
 
 

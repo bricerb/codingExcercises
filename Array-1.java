fc2948c040dc2fac86d82c47b68e762c0b2456c5
Richard Blanch <bricerb@gmail.com>
2017-01-01 20:01:31 -0500

    Solved makeLast

--- Array-1.java
+++ Array-1.java
@@ -106,6 +106,15 @@ public boolean no23(int[] nums) {
   return !(nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3);
 }
 
+// Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more. Note: by default, a new int array contains all 0's.
+
+public int[] makeLast(int[] nums) {
+  int[] res = new int[nums.length * 2];
+  res[res.length-1] = nums[nums.length - 1];
+  return res;
+}
+
+
 
 
 

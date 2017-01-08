2d889e23ad924bc6d998574d85eb72495fa72657
Richard Blanch <bricerb@gmail.com>
2017-01-07 20:27:55 -0500

    Solved firstChar

--- Map-2.java
+++ Map-2.java
@@ -42,6 +42,19 @@ public Map<String, Integer> wordCount(String[] strings) {
   return map;
 }
 
+// Given an array of non-empty strings, return a Map<String, String> with a key for every different first character seen, with the value of all the strings starting with that character appended together in the order they appear in the array.
+
+public Map<String, String> firstChar(String[] strings) {
+  Map<String, String> map = new HashMap<String, String>();
+  for (String s : strings) {
+    if (map.containsKey(s.substring(0,1))) {
+      map.put(s.substring(0,1), map.get(s.substring(0,1)) + s);
+    } else {
+      map.put(s.substring(0,1), s);
+    }
+  }
+  return map;
+}
 
 
 

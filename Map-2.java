191612a525b464e018b7df8b32ac9c112950df20
Richard Blanch <bricerb@gmail.com>
2017-01-08 15:51:49 -0500

    Solved allSwap

--- Map-2.java
+++ Map-2.java
@@ -86,6 +86,24 @@ public Map<String, Boolean> wordMultiple(String[] strings) {
   return map;
 }
 
+// We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. When a position in the array has been swapped, it no longer matches anything. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+
+public String[] allSwap(String[] strings) {
+  int i = 0;
+  Map<String, Integer> map = new HashMap<String, Integer>();
+  for (String s : strings) {
+    if (map.containsKey(s.substring(0,1))) {
+      String temp = strings[map.get(s.substring(0,1))];
+      strings[map.get(s.substring(0,1))] = s;
+      strings[i] = temp;
+      map.remove(s.substring(0,1));
+    } else {
+      map.put(s.substring(0,1), i);
+    }
+    i++;
+  }
+  return strings;
+}
 
 
 

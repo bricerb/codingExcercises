6f88e903c10b5adb60d1e8092bdacde048972cd1
Richard Blanch <bricerb@gmail.com>
2017-01-08 15:25:37 -0500

    Solved wordMultiple

--- Map-2.java
+++ Map-2.java
@@ -72,6 +72,22 @@ public String wordAppend(String[] strings) {
   return res.toString();
 }
 
+// Given an array of strings, return a Map<String, Boolean> where each different string is a key and its value is true if that string appears 2 or more times in the array.
+
+public Map<String, Boolean> wordMultiple(String[] strings) {
+  Map<String, Boolean> map = new HashMap<String, Boolean>();
+  for (String s : strings) {
+    if (map.containsKey(s)) {
+      map.put(s, true);
+    } else {
+      map.put(s, false);
+    }
+  }
+  return map;
+}
+
+
+
 
 
 

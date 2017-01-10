463aa6adff166639e6b95f24eab6e8ffbad4a4ea
Richard Blanch <bricerb@gmail.com>
2017-01-10 14:14:58 -0500

    Solved ndOther

--- String-2.java
+++ String-2.java
@@ -56,6 +56,26 @@ public int countCode(String str) {
   return count;
 }
 
+// Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+public boolean endOther(String a, String b) {
+  a = a.toLowerCase();
+  b = b.toLowerCase();
+  if (a.length() > b.length()) {
+    if (a.substring(a.length() - b.length()).equals(b)){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (b.substring(b.length() - a.length()).equals(a)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 
 
 

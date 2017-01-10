70b6396bad4e537e2180afe74b3b9281c5bc9a76
Richard Blanch <bricerb@gmail.com>
2017-01-10 13:02:43 -0500

    Solved blackjack

--- Logic-2.java
+++ Logic-2.java
@@ -111,7 +111,35 @@ public boolean closeFar(int a, int b, int c) {
   return false;
 }
 
+// Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if they both go over.
 
+public int blackjack(int a, int b) {
+  if (a > 21) {
+    a = 0;
+  }
+  if (b > 21) {
+    b = 0;
+  }
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// ALTERNATIVE - Solve blackjack() using ternary operator.
+
+public int blackjack(int a, int b) {
+  if (a > 21) {
+    a = 0;
+  }
+  if (b > 21) {
+    b = 0;
+  }
+  int res;
+  res = (a > b) ? a : b;
+  return res;
+}
 
 
 

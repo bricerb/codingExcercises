4f8f7b636efe65bf6a28678c4a406b6380028797
Richard Blanch <bricerb@gmail.com>
2017-01-15 12:37:32 -0500

    Solved prefixAgain

--- String-2.java
+++ String-2.java
@@ -213,6 +213,18 @@ public String repeatSeparator(String word, String sep, int count) {
   return res.toString();
 }
 
+// Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length().
+
+public boolean prefixAgain(String str, int n) {
+  boolean res = false;
+  String comparor = str.substring(0, n);
+  for (int i = n; i <= str.length()-n; i++) {
+    if (str.substring(i, i+n).equals(comparor)) {
+      res = true;
+    }
+  }
+  return res;
+}
 
 
 

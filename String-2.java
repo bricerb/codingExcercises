07947f609b536258a9cb58f18a3b21ff772acd8d
Richard Blanch <bricerb@gmail.com>
2017-01-21 12:05:38 -0500

    solved zipZap

--- String-2.java
+++ String-2.java
@@ -299,6 +299,23 @@ public String oneTwo(String str) {
   return res.toString();
 }
 
+// Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
 
+public String zipZap(String str) {
+  if (str.length() < 3) {
+    return str;
+  }
+  StringBuilder res = new StringBuilder();
+  for (int i = 0; i < str.length(); i++) {
+    if (str.substring(i, i + 1).equals("z") && str.substring(i + 2, i +3).equals("p")) {
+      res.append(str.substring(i, i +1));
+      res.append(str.substring(i + 2, i + 3));
+      i += 2;
+    } else {
+      res.append(str.substring(i, i + 1));
+    }
+  }
+  return res.toString();
+}
 
 

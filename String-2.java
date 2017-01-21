dc0685dd15b7b683282821d4b34a4362b0988fce
Richard Blanch <bricerb@gmail.com>
2017-01-21 00:42:54 -0500

    Solved xyzMiddle

--- String-2.java
+++ String-2.java
@@ -226,8 +226,26 @@ public boolean prefixAgain(String str, int n) {
   return res;
 }
 
+// Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of chars to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
 
-
+public boolean xyzMiddle(String str) {
+  if (str.length() < 3) {
+    return false;
+  }
+  if (str.equals("xyz")) {
+    return true;
+  }
+  for (int i = 0; i < str.length() - 2; i++) {
+    if (str.substring(i, i + 3).equals("xyz")) {
+      int pre = i;
+      int suf = i + 3;
+      if (Math.abs(pre - (str.length() - suf)) <= 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 
 

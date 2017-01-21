711001bf1ad3d3f2c54cef22e1d96bcaa99e5350
Richard Blanch <bricerb@gmail.com>
2017-01-21 01:15:18 -0500

    Solved oneTwo

--- String-2.java
+++ String-2.java
@@ -284,6 +284,21 @@ public boolean sameStarChar(String str) {
   return res;
 }
 
+// Given a string, compute a new string by moving the first char to come after the next two chars, so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd". Ignore any group of fewer than 3 chars at the end.
+
+public String oneTwo(String str) {
+  StringBuilder res = new StringBuilder();
+  for (int i = 0; i < str.length() - 2; i +=3) {
+    if (str.length() - i < 3) {
+      res.append(str.substring(i, str.length()));
+    } else {
+      res.append(str.substring(i + 1, i + 3));
+      res.append(str.substring(i, i + 1));
+    }
+  }
+  return res.toString();
+}
+
 
 
 

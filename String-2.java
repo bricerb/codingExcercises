b15b2adc97c5a2612edb44b641881ee6cdafa5e0
Richard Blanch <bricerb@gmail.com>
2017-01-23 00:28:56 -0500

    Solved wordEnds

--- String-2.java
+++ String-2.java
@@ -352,6 +352,22 @@ public String plusOut(String str, String word) {
   return sb.toString();
 }
 
+// Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
+
+public String wordEnds(String str, String word) {
+  StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < str.length(); i++) {
+    if (i > 0 && i + word.length() <= str.length() && str.substring(i, i + word.length()).equals(word)) {
+      sb.append(str.charAt(i - 1));
+    }
+    if (i + word.length() <= str.length() - 1 &&  str.substring(i, i + word.length()).equals(word)) {
+      sb.append(str.charAt(i + word.length()));
+    }
+  }
+  return sb.toString();
+}
+
+
 
 
 

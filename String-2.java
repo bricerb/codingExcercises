5400e9f3e443c9c6db880c6a321593753f999153
Richard Blanch <bricerb@gmail.com>
2017-01-22 23:48:26 -0500

    Solved starOut

--- String-2.java
+++ String-2.java
@@ -318,4 +318,31 @@ public String zipZap(String str) {
   return res.toString();
 }
 
+// Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+public String starOut(String str) {
+  StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < str.length(); i++) {
+    if (str.charAt(i) == '*') {
+      continue;
+    }
+    if (i > 0 && str.charAt(i - 1) == '*') {
+      continue;
+    }
+    if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+      continue;
+    }
+    sb.append(str.charAt(i));
+  }
+  return sb.toString();
+}
+
+
+
+
+
+
+
+
+
 

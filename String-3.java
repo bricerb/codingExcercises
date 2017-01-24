36e51c9c9be1ce988c3d4b82fd25926bd77edaa5
Richard Blanch <bricerb@gmail.com>
2017-01-23 22:19:10 -0500

    Solved equalIsNot

--- String-3.java
+++ String-3.java
@@ -28,6 +28,21 @@ public String withoutString(String base, String remove) {
   return sb.toString();
 }
 
+// Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances of "not" anywhere in the string (case sensitive).
+
+public boolean equalIsNot(String str) {
+  int is = 0;
+  int not = 0;
+  for (int i = 0; i < str.length(); i++) {
+    if (i + 2 <= str.length() && str.substring(i, i + 2).equals("is")) {
+      is ++;
+    }
+    if (i + 3 <= str.length() && str.substring(i, i + 3).equals("not")) {
+      not ++;
+    }
+  }
+  return (is == not);
+}
 
 
 

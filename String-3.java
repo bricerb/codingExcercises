08c4d28bc3aeb1eb589844f3de78185b34941b50
Richard Blanch <bricerb@gmail.com>
2017-01-23 22:04:58 -0500

    Solved withoutString

--- String-3.java
+++ String-3.java
@@ -11,3 +11,29 @@ public int countYZ(String str) {
   }
   return counter;
 }
+
+// Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+
+public String withoutString(String base, String remove) {
+  StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < base.length(); i++) {
+    if (i + remove.length() <= base.length()) {
+      if (base.substring(i, i + remove.length()).toLowerCase().equals(remove.toLowerCase())) {
+        i += remove.length() - 1;
+        continue;
+      }
+    }
+    sb.append(base.charAt(i));
+  }
+  return sb.toString();
+}
+
+
+
+
+
+
+
+
+
+

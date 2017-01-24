7edc2d60555fdf609e9334fc5d95b7a78d499874
Richard Blanch <bricerb@gmail.com>
2017-01-23 22:51:28 -0500

    Solved countTriple

--- String-3.java
+++ String-3.java
@@ -60,7 +60,19 @@ public boolean gHappy(String str) {
   return isHappy;
 }
 
+// We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples in the given string. The triples may overlap.
 
+public int countTriple(String str) {
+  int len = str.length();
+  int count = 0;
+  for (int i = 0; i < len - 2; i++) {
+    char tmp = str.charAt(i);
+    if (tmp == str.charAt(i + 1) && tmp == str.charAt(i + 2)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 
 

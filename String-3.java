c8899e808a56f8d9b88c04a438d7b2c02f1ad67f
Richard Blanch <bricerb@gmail.com>
2017-01-29 18:59:49 -0500

    solved sumNumbers

--- String-3.java
+++ String-3.java
@@ -138,6 +138,26 @@ public int maxBlock(String str) {
   return count;
 }
 
+// Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+
+public int sumNumbers(String str) {
+  int sum = 0;
+  StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < str.length(); i++) {
+    if (Character.isDigit(str.charAt(i))) {
+      sb.append(str.charAt(i));
+    } else {
+      if (sb.toString().length() > 0) {
+        sum += Integer.parseInt(sb.toString());
+        sb = new StringBuilder();
+      }
+    }
+    if (i == str.length() - 1 && sb.toString().length() > 0) {
+      sum += Integer.parseInt(sb.toString());
+    }
+  }
+  return sum;
+}
 
 
 

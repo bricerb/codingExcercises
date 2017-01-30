8ac4343609679dad8e564fb79cdfc89ca57b038a
Richard Blanch <bricerb@gmail.com>
2017-01-29 20:42:03 -0500

    Solved has22

--- Array-2.java
+++ Array-2.java
@@ -70,3 +70,27 @@ public int sum67(int[] nums) {
   return sum;
 }
 
+// Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+
+public boolean has22(int[] nums) {
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] == 2 && nums[i + 1] == 2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

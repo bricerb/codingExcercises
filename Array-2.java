ae29da45d59d97dbc8529af8f8b333dcd7f2d6d9
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:27:35 -0500

    Solved fizzArray2

--- Array-2.java
+++ Array-2.java
@@ -142,6 +142,17 @@ public boolean only14(int[] nums) {
   return oneFour;
 }
 
+// Given a number n, create and return a new string array of length n, containing the strings "0", "1" "2" .. through n-1. N may be 0, in which case just return a length 0 array. Note: String.valueOf(xxx) will make the String form of most types. The syntax to make a new string array is: new String[desired_length] 
+
+public String[] fizzArray2(int n) {
+  String[] res = new String[n];
+  for (int i = 0; i < res.length; i++) {
+    res[i] = String.valueOf(i);
+  }
+  return res;
+}
+
+
 
 
 

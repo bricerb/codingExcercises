5fafdb60ba39e62ad52bd6e534d4e6f6a66124cf
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:40:11 -0500

    Solved isEverywhere

--- Array-2.java
+++ Array-2.java
@@ -168,6 +168,17 @@ public boolean no14(int[] nums) {
   return (ones || fours);
 }
 
+// We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array, at least one of the pair is that value. Return true if the given value is everywhere in the array.
+
+public boolean isEverywhere(int[] nums, int val) {
+  boolean isEverywhere = true;
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (!(nums[i] == val || nums[i + 1] == val)) {
+      isEverywhere = false;
+    }
+  }
+  return isEverywhere;
+}
 
 
 

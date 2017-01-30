889404f24128252fbc5e84f1774b085afb8bd144
Richard Blanch <bricerb@gmail.com>
2017-01-29 20:05:48 -0500

    Solved centeredAverage

--- Array-2.java
+++ Array-2.java
@@ -21,3 +21,22 @@ public int bigDiff(int[] nums) {
   }
   return max - min;
 }
+
+// Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+
+public int centeredAverage(int[] nums) {
+  int max = nums[0];
+  int min = nums[0];
+  int mean = 0;
+  for (int i  = 0; i < nums.length; i++) {
+    max = Math.max(max, nums[i]);
+    min = Math.min(min, nums[i]);
+  }
+  for (int i = 0; i < nums.length; i++) {
+    mean += nums[i];
+  }
+  return (mean - (max + min)) / (nums.length - 2) ;
+}
+
+
+

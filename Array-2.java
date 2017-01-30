8dd52713012170a92428f1b49f064f39f8e91bf8
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:16:49 -0500

    Solved only14

--- Array-2.java
+++ Array-2.java
@@ -130,6 +130,18 @@ public int[] fizzArray(int n) {
   return res;
 }
 
+// Given an array of ints, return true if every element is a 1 or a 4.
+
+public boolean only14(int[] nums) {
+  boolean oneFour = true;
+  for (int i = 0; i < nums.length; i++) {
+    if (!(nums[i] == 1 || nums[i] == 4)) {
+      oneFour = false;
+    }
+  }
+  return oneFour;
+}
+
 
 
 

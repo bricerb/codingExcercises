4bb20e779ff1396f2f27b094a6a628570598854f
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:11:16 -0500

    Solved fizzArray

--- Array-2.java
+++ Array-2.java
@@ -120,6 +120,15 @@ public boolean more14(int[] nums) {
   return (ones > fours);
 }
 
+// Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works. The syntax to make a new int array is: new int[desired_length]
+
+public int[] fizzArray(int n) {
+  int[] res = new int[n];
+  for (int i = 0; i < res.length; i++) {
+    res[i] = i;
+  }
+  return res;
+}
 
 
 

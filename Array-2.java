a1649d5c72362de56d010006d4fb7863154a8f37
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:33:13 -0500

    Solved no14

--- Array-2.java
+++ Array-2.java
@@ -152,6 +152,21 @@ public String[] fizzArray2(int n) {
   return res;
 }
 
+// Given an array of ints, return true if it contains no 1's or it contains no 4's.
+
+public boolean no14(int[] nums) {
+  boolean ones = true;
+  boolean fours = true;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      ones = false;
+    }
+    if (nums[i] == 4) {
+      fours = false;
+    }
+  }
+  return (ones || fours);
+}
 
 
 

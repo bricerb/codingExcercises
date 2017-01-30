1f29ea13a9cb0e9498002fa0f33f68f8858f5eea
Richard Blanch <bricerb@gmail.com>
2017-01-30 13:07:31 -0500

    Solved sum28

--- Array-2.java
+++ Array-2.java
@@ -92,7 +92,17 @@ public boolean lucky13(int[] nums) {
   return true;
 }
 
+// Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
 
+public boolean sum28(int[] nums) {
+  int sum = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      sum += 2;
+    }
+  }
+  return (sum == 8);
+}
 
 
 

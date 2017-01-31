e090c32e9b71e713f17c26c67724239bab113940
Richard Blanch <bricerb@gmail.com>
2017-01-31 12:32:58 -0500

    Solved shiftLeft

--- Array-2.java
+++ Array-2.java
@@ -340,6 +340,20 @@ public int[] fizzArray3(int start, int end) {
   return nums;
 }
 
+// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
+
+public int[] shiftLeft(int[] nums) {
+  int[] newNums = new int[nums.length];
+  for (int i = 0; i < nums.length; i++) {
+    if (i == nums.length - 1) {
+      newNums[i] = nums[0];
+    } else {
+      newNums[i] = nums[i + 1];
+    }
+  }
+  return newNums;
+}
+
 
 
 

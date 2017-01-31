55e4d3076ea9564bc3fef1af6df5641a5d787ed7
Richard Blanch <bricerb@gmail.com>
2017-01-31 11:58:06 -0500

    Solved twoTwo

--- Array-2.java
+++ Array-2.java
@@ -279,7 +279,23 @@ public boolean haveThree(int[] nums) {
   return has3;
 }
 
+// Given an array of ints, return true if every 2 that appears in the array is next to another 2.
 
+public boolean twoTwo(int[] nums) {
+  boolean twos = true;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 2) {
+      if (i < nums.length - 2 && nums[i + 1] == 2) {
+        twos = true;
+      } else if (i > 0 && nums[i - 1] == 2) {
+        twos = true;
+      } else {
+        twos = false;
+      }
+    }
+  }
+  return twos;
+}
 
 
 

969d32fe7ca385678fefda2d9a99ea817c142185
Richard Blanch <bricerb@gmail.com>
2017-01-31 12:30:47 -0500

    Solved fizzArray3

--- Array-2.java
+++ Array-2.java
@@ -330,6 +330,15 @@ public boolean tripleUp(int[] nums) {
   return triple;
 }
 
+// Given start and end numbers, return a new array containing the sequence of integers from start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. The end number will be greater or equal to the start number. Note that a length-0 array is valid.
+
+public int[] fizzArray3(int start, int end) {
+  int[] nums = new int[end - start];
+  for (int i = 0; i < nums.length; i++) {
+    nums[i] = start + i;
+  }
+  return nums;
+}
 
 
 

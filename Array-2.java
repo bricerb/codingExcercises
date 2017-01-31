198c426096ad1e3fbb2a3797abce0f147db0607b
Richard Blanch <bricerb@gmail.com>
2017-01-31 11:28:30 -0500

    Solved haveThree

--- Array-2.java
+++ Array-2.java
@@ -260,6 +260,25 @@ public boolean modThree(int[] nums) {
   return mod3;
 }
 
+// Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+
+public boolean haveThree(int[] nums) {
+  boolean has3 = false;
+  int count3 = 0;
+  for (int i = 0; i < nums.length ; i++) {
+    if (i < nums.length - 1 && nums[i] == 3 && nums[i + 1] == 3) {
+      return false;
+    }
+    if (nums[i] == 3) {
+      count3++;
+    }
+  }
+  if (count3 == 3) {
+    has3 = true;
+  }
+  return has3;
+}
+
 
 
 

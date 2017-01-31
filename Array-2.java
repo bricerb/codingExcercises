9e42a2e1199da3e28ae73e3611a22585b394c4cd
Richard Blanch <bricerb@gmail.com>
2017-01-31 11:04:02 -0500

    Solved has77

--- Array-2.java
+++ Array-2.java
@@ -211,6 +211,21 @@ public int matchUp(int[] nums1, int[] nums2) {
   return count;
 }
 
+// Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's separated by one element, such as with {7, 1, 7}.
+
+public boolean has77(int[] nums) {
+  boolean sevens = false;
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (i < nums.length - 2 && nums[i] == 7 && nums[i + 2] == 7) {
+      sevens = true;
+    }
+    if (nums[i] == 7 && nums[i + 1] == 7) {
+      sevens = true;
+    }
+  }
+  return sevens;
+}
+
 
 
 

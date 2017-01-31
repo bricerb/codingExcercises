1728ea622bb50e441154b824d953ef296533be3b
Richard Blanch <bricerb@gmail.com>
2017-01-31 11:15:53 -0500

    Solved mod3

--- Array-2.java
+++ Array-2.java
@@ -242,6 +242,24 @@ public boolean has12(int[] nums) {
   return has12;
 }
 
+// Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+
+public boolean modThree(int[] nums) {
+  boolean mod3 = false;
+  if (nums.length < 3) {
+    return mod3;
+  }
+  for (int i = 0; i < nums.length - 2; i++) {
+    if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+      mod3 = true;
+    }
+    if (nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+      mod3 = true;
+    }
+  }
+  return mod3;
+}
+
 
 
 

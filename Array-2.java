aab0f77f015c6a16000434e7e7f4e8b97d1ff0c3
Richard Blanch <bricerb@gmail.com>
2017-01-31 12:56:24 -0500

    Solved tenRun

--- Array-2.java
+++ Array-2.java
@@ -354,6 +354,28 @@ public int[] shiftLeft(int[] nums) {
   return newNums;
 }
 
+// For each multiple of 10 in the given array, change all the values following it to be that multiple of 10, until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+
+public int[] tenRun(int[] nums) {
+  int[] newNums = new int[nums.length];
+  boolean hasTemp = false;
+  int temp = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] % 10 == 0) {
+      temp = nums[i];
+      hasTemp = true;
+    }
+    if (hasTemp == false) {
+      newNums[i] = nums[i];
+    } else {
+      newNums[i] = temp;
+    }
+  }
+  return newNums;
+}
+
+
+
 
 
 

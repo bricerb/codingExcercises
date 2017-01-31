c7c24497941b9a9b758e4ecae1b0a9038cc035dc
Richard Blanch <bricerb@gmail.com>
2017-01-31 12:15:34 -0500

    Solved sameEnds

--- Array-2.java
+++ Array-2.java
@@ -297,6 +297,24 @@ public boolean twoTwo(int[] nums) {
   return twos;
 }
 
+// Return true if the group of N numbers at the start and end of the array are the same. For example, with {5, 6, 45, 99, 13, 5, 6}, the ends are the same for n=0 and n=2, and false for n=1 and n=3. You may assume that n is in the range 0..nums.length inclusive
+
+public boolean sameEnds(int[] nums, int len) {
+  boolean sameEnds = true;
+  int[] last = new int[len];
+  for (int i = 0; i < len; i ++) {
+    last[i] = nums[nums.length - len + i];
+  }
+  for (int i = 0; i < last.length; i++) {
+    if (nums[i] == last[i]) {
+      sameEnds = true;
+    } else {
+      return false;
+    }
+  }
+  return sameEnds;
+}
+
 
 
 

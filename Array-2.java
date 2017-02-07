128034346759bea4f12e7673a250068b6a45d6e0
Richard Blanch <bricerb@gmail.com>
2017-02-06 21:27:19 -0500

    Solved post4

--- Array-2.java
+++ Array-2.java
@@ -390,6 +390,25 @@ public int[] pre4(int[] nums) {
   return res;
 }
 
+// Given a non-empty array of ints, return a new array containing the elements from the original array that come after the last 4 in the original array. The original array will contain at least one 4. Note that it is valid in java to create an array of length 0.
+
+public int[] post4(int[] nums) {
+  int[] res = null;
+  int tempIndex = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 4) {
+      tempIndex = i;
+    }
+  }
+  res = new int[nums.length - tempIndex - 1];
+  for (int i = 0; i < res.length; i++) {
+    res[i] = nums[nums.length - res.length + i];
+  }
+  return res;
+}
+
+
+
 
 
 

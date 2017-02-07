cec037d102ef3856e5d544bf86f0a090d186cbfa
Richard Blanch <bricerb@gmail.com>
2017-02-06 19:56:07 -0500

    Solved pre4

--- Array-2.java
+++ Array-2.java
@@ -374,7 +374,21 @@ public int[] tenRun(int[] nums) {
   return newNums;
 }
 
+// Given a non-empty array of ints, return a new array containing the elements from the original array that come before the first 4 in the original array. The original array will contain at least one 4. Note that it is valid in java to create an array of length 0.
 
+public int[] pre4(int[] nums) {
+  int[] res = null;
+  for (int i = 0; i < nums.length; i++) {
+     if (nums[i] == 4) {
+       res = new int[i];
+       break;
+     }
+  }
+  for (int i = 0; i < res.length; i++) {
+    res[i] = nums[i];
+  }
+  return res;
+}
 
 
 

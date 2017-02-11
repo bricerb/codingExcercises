7c3f9e0449f738faaaf055b651f64761ba15c0a7
Richard Blanch <bricerb@gmail.com>
2017-02-11 09:27:11 -0500

    Solved fix45

--- Array-3.java
+++ Array-3.java
@@ -39,6 +39,24 @@ public int[] fix34(int[] nums) {
   return nums;
 }
 
+// (This is a slightly harder version of the fix34 problem.) Return an array that contains exactly the same numbers as the given array, but rearranged so that every 4 is immediately followed by a 5. Do not move the 4's, but every other number may move. The array contains the same number of 4's and 5's, and every 4 has a number after it that is not a 4. In this version, 5's may appear anywhere in the original array.
+
+public int[] fix45(int[] nums) {
+  int last4 = 0;
+  for (int i = 0; i < nums.length; i ++) {
+    if (nums[i] == 5) {
+      for (int j = last4; j < nums.length; j ++) {
+        if (nums[j] == 4 && !(nums[j+1] == 5)) {
+          int temp = nums[j+1];
+          nums[j+1] = nums[i];
+          nums[i] = temp;
+          break;
+        }
+      }
+    }
+  }
+  return nums;
+}
 
 
 

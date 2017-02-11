9c6ef607b9d4971e902a98d24ef55d133e31811b
Richard Blanch <bricerb@gmail.com>
2017-02-11 09:39:33 -0500

    second alternative for fix45

--- Array-3.java
+++ Array-3.java
@@ -79,6 +79,26 @@ public int[] fix45(int[] nums) {
 // return nums;
 // }
 
+// Solution for fix45 without index or break;
+
+// public int[] fix45(int[] nums) {
+// 	for(int i=0;i<nums.length-1;i++){
+// 		if(nums[i]==4 &&nums[i+1]!=5){
+// 			for(int j=0;j<nums.length;j++){
+// 				if(nums[j]==5 && j==0){
+// 					nums[0]=nums[i+1];
+// 					nums[i+1]=5;
+// 				}
+// 				if(nums[j]==5 && nums[j-1]!=4){
+// 					nums[j]=nums[i+1];
+// 					nums[i+1]=5;
+// 				}
+// 			}
+// 		}
+// 	}
+// return nums;
+// }
+
 
 
 

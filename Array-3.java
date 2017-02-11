3373a0bb64ee54fab9115321b5f661b182d0d95e
Richard Blanch <bricerb@gmail.com>
2017-02-11 09:07:26 -0500

    Solved fix34

--- Array-3.java
+++ Array-3.java
@@ -19,6 +19,26 @@ public int maxSpan(int[] nums) {
   return counter;
 }
 
+// Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+
+public int[] fix34(int[] nums) {
+  int last4 = 0;
+  for (int i = 0; i < nums.length; i ++) {
+    if (nums[i] == 3) {
+      for (int j = last4; j < nums.length; j ++) {
+        if (nums[j] == 4) {
+          int temp = nums[i+1];
+          nums[i+1] = nums[j];
+          nums[j] = temp;
+          last4 = j;
+          break;
+        }
+      }
+    }
+  }
+  return nums;
+}
+
 
 
 

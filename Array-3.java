3101ce9164dbe300f9bcef209f27fe8969cd6ec1
Richard Blanch <bricerb@gmail.com>
2017-02-16 01:11:39 -0500

    Solved seriesUp

--- Array-3.java
+++ Array-3.java
@@ -154,6 +154,19 @@ public int[] squareUp(int n) {
   return res;
 }
 
+// Given n>=0, create an array with the pattern {1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n} (spaces added to show the grouping). Note that the length of the array will be 1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2.
+
+public int[] seriesUp(int n) {
+  int[] res = new int[n*(n+1)/2];
+  int index = 0;
+  for (int i = 1; i <= n; i ++) {
+    for (int j = 1; j <= i; j ++) {
+      res[index++] = j;
+    }
+  }
+  return res;
+}
+
 
 
 

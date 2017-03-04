332b0d28cd5e5fde0524824d7fcaca36f84f1e0a
Richard Blanch <bricerb@gmail.com>
2017-03-04 14:23:56 -0500

    Solved wordsCount

--- Ap-1.java
+++ Ap-1.java
@@ -56,6 +56,19 @@ public int average(int[] scores, int start, int end) {
   return averages / (scores.length/2);
 }
 
+// Given an array of strings, return the count of the number of strings with the given length.
+
+public int wordsCount(String[] words, int len) {
+  int count = 0;
+  for (int i = 0; i < words.length; i ++) {
+    if (words[i].length() == len) {
+      count ++;
+    }
+  }
+  return count;
+}
+
+
 
 
 

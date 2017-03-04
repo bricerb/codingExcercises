f1cd16e7d951efea114ab756da3c765165ed28ce
Richard Blanch <bricerb@gmail.com>
2017-03-04 14:26:58 -0500

    Solved wordsFront

--- Ap-1.java
+++ Ap-1.java
@@ -68,6 +68,15 @@ public int wordsCount(String[] words, int len) {
   return count;
 }
 
+// Given an array of strings, return a new array containing the first N strings. N will be in the range 1..length.
+
+public String[] wordsFront(String[] words, int n) {
+  String[] res = new String[n];
+  for (int i = 0; i < n; i ++) {
+    res[i] = words[i];
+  }
+  return res;
+}
 
 
 

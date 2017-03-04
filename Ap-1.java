821e61a7ee86304cd0a439d95b99ddf776692c29
Richard Blanch <bricerb@gmail.com>
2017-03-04 14:34:19 -0500

    Solved hasOne

--- Ap-1.java
+++ Ap-1.java
@@ -91,6 +91,19 @@ public List wordsWithoutList(String[] words, int len) {
 }
 
 
+// Given a positive int n, return true if it contains a 1 digit. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+public boolean hasOne(int n) {
+  while (n > 0) {
+    if (n % 10 == 1) {
+      return true;
+    }
+    n = n / 10;
+  }
+  return false;
+}
+
+
 
 
 

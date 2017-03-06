69ee9be95de1f7ad83a927ab3a1e34aac46420a4
Richard Blanch <bricerb@gmail.com>
2017-03-05 21:05:02 -0500

    Solved copyEvents

--- Ap-1.java
+++ Ap-1.java
@@ -123,6 +123,22 @@ public boolean dividesSelf(int n) {
   return isTrue;
 }
 
+// Given an array of positive ints, return a new array of length "count" containing the first even numbers from the original array. The original array will contain at least "count" even numbers.
+
+public int[] copyEvens(int[] nums, int count) {
+  int[] res = new int[count];
+  int index = 0;
+  for (int i = 0; i < nums.length; i ++) {
+    if (index == count) {
+      break;
+    }
+    if (nums[i] % 2 == 0) {
+      res[index] = nums[i];
+      index ++;
+    }
+  }
+  return res;
+}
 
 
 

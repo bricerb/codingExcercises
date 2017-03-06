a8985aea27736ba7bac7467a0b03df5b09355fc7
Richard Blanch <bricerb@gmail.com>
2017-03-06 13:07:48 -0500

    Solved scoreUp

--- Ap-1.java
+++ Ap-1.java
@@ -180,5 +180,20 @@ public int matchUp(String[] a, String[] b) {
   return count;
 }
 
+// The "key" array is an array containing the correct answers to an exam, like {"a", "a", "b", "b"}. the "answers" array contains a student's answers, with "?" representing a question left blank. The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer.
+
+public int scoreUp(String[] key, String[] answers) {
+  int res = 0;
+  for (int i = 0; i < key.length; i ++) {
+    if (answers[i] == "?") {
+      continue;
+    } else if (key[i] == answers[i]) {
+      res += 4;
+    } else {
+      res -= 1;
+    }
+  }
+  return res;
+}
 
 

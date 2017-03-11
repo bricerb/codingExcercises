0297b62c3a8cbf1805e1eccf36b9c7bc2940e585
Richard Blanch <bricerb@gmail.com>
2017-03-10 21:12:44 -0500

    Solved scoresSepcial

--- Ap-1.java
+++ Ap-1.java
@@ -218,4 +218,19 @@ public String[] wordsWithout(String[] words, String target) {
   return res;
 }
 
+// Given two arrays, A and B, of non-negative int scores. A "special" score is one which is a multiple of 10, such as 40 or 90. Return the sum of largest special score in A and the largest special score in B. To practice decomposition, write a separate helper method which finds the largest special score in an array. Write your helper method after your scoresSpecial() method in the JavaBat text area.
+
+public int scoresSpecial(int[] a, int[] b) {
+  return (findSpecial(a) + findSpecial(b));
+}
+
+public int findSpecial(int[] n) {
+  int highest = 0;
+  for (int i = 0; i < n.length; i ++) {
+    if (n[i] > highest && n[i] % 10 == 0) {
+      highest = n[i];
+    }
+  }
+  return highest;
+}
 

bdbbc74bb7d6a95143151cad58a3c958ec7a8cc5
Richard Blanch <bricerb@gmail.com>
2017-03-10 21:09:31 -0500

    Solved wordsWithout

--- Ap-1.java
+++ Ap-1.java
@@ -196,4 +196,26 @@ public int scoreUp(String[] key, String[] answers) {
   return res;
 }
 
+// Given an array of strings, return a new array without the strings that are equal to the target string. One approach is to count the occurrences of the target string, make a new array of the correct length, and then copy over the correct strings.
+
+public String[] wordsWithout(String[] words, String target) {
+  int count = 0;
+  for (int i = 0; i < words.length; i ++) {
+    if (words[i].equals(target)) {
+      continue;
+    }
+    count ++;
+  }
+  String[] res = new String[count];
+  int index = 0;
+  for (int i = 0; i < words.length; i ++) {
+    if (words[i].equals(target)) {
+      continue;
+    }
+    res[index] = words[i];
+    index ++;
+  }
+  return res;
+}
+
 
